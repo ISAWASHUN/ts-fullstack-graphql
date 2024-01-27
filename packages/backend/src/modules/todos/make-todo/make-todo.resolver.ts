2c87204f5e03319d2720b60e5ebe87710f4b6acf
ISAWASHUN <syun20000616@icloud.com>
2024-01-27 12:36:11 +0900

    make-todoの完了

--- packages/backend/src/modules/todos/make-todo/make-todo.resolver.ts
+++ packages/backend/src/modules/todos/make-todo/make-todo.resolver.ts
@@ -2,15 +2,19 @@ import { Resolvers } from "../../../__generated__/graphql.js";
 
 export const resolvers: Resolvers = {
   Mutation: {
-    makeTodo: async (_, {makeTodoInput}, context, info) => {
-      const todoItem = {
-        id: crypto.randomUUID(),
-        title: makeTodoInput.title,
-        updatedAt: new Date().toISOString(),
-        createdAt: new Date().toISOString(),
-      }
+    makeTodo: async (_, {makeTodoInput}, {prismaClient}, info) => {
+      const newTodo = await prismaClient.todo.create({
+        data: {
+          title: makeTodoInput.title,
+        },
+      });
+      newTodo.createdAt
       return {
-        todo: todoItem
+        todo: {
+          ...newTodo,
+          createdAt: newTodo.createdAt.toISOString(),
+          updatedAt: newTodo.updatedAt.toISOString(),
+        }
       }
     }
   }
