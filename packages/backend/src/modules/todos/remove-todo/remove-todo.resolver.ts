65e249557f9d68475e4805c1cfe1a75ebfe0b197
ISAWASHUN <syun20000616@icloud.com>
2024-01-27 13:34:27 +0900

    remove-todoの作成

---
+++ packages/backend/src/modules/todos/remove-todo/remove-todo.resolver.ts
@@ -0,0 +1,32 @@
+import { GraphQLError } from "graphql";
+import { MyContext } from "../../../../types/graphql.js";
+import { Resolvers } from "../../../__generated__/graphql.js";
+
+export const resolvers: Resolvers<MyContext> = {
+  Mutation: {
+    removeTodo: async (_, {removeTodoInput}, {prismaClient}, info) => {
+      const existingTodo = await prismaClient.todo.findUnique({
+        where: {
+          id: removeTodoInput.todoId
+        }
+      });
+
+      if(!existingTodo) {
+        throw new GraphQLError("Todo not found");
+      }
+
+      await prismaClient.todo.delete({
+        where: {
+          id: existingTodo.id
+        }
+      });
+      return {
+        todo: {
+          ...existingTodo,
+          createdAt: existingTodo.createdAt.toISOString(),
+          updatedAt: existingTodo.updatedAt.toISOString(),
+        }
+      }
+    }
+  }
+}
